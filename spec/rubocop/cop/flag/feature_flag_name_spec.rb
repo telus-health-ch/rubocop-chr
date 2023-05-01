0eefbe0a2132b0d90d4040a26efe00d227a89ecd
Rafael Rocha <rafael.rocha@realsure.com>
2023-05-01 17:53:27 -0300

    Fix github ci

--- spec/rubocop/cop/flag/feature_flag_name_spec.rb
+++ spec/rubocop/cop/flag/feature_flag_name_spec.rb
@@ -4,9 +4,6 @@ RSpec.describe RuboCop::Cop::Flag::FeatureFlagName, :config do
   let(:config) { RuboCop::Config.new }
 
   it 'registers an offense when using an invalid feature flag name' do
-    # An error occurred while CustomCops/FeatureFlagName cop was
-    # inspecting /Users/rafaelrocha/src/telus/chr-backend/app/models/
-    # respondent/searchable_value.rb:32:11
     expect_offense(<<~RUBY)
       if FeatureFlag.enabled? 'SEARCH_MY_FLAG_ID'
          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use snake_case with lowercase for feature flag names.
