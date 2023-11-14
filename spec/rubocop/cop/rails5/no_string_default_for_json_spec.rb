2ce71e86d4a504f7dc842616182b8118081f9905
Eduardo Resende <ed.resende@gmail.com>
2023-11-14 09:49:12 -0300

    Add tests for objects as default

--- spec/rubocop/cop/rails5/no_string_default_for_json_spec.rb
+++ spec/rubocop/cop/rails5/no_string_default_for_json_spec.rb
@@ -26,4 +26,14 @@ RSpec.describe RuboCop::Cop::Rails5::NoStringDefaultForJson, :config do
       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Do not use string as a default value for JSON or JSONB columns. Column: archive_data
     RUBY
   end
+
+  it 'does not register offenses when using objects as default values' do
+    expect_no_offenses(<<~RUBY)
+      add_column :appointments, :more_data, :json, default: []
+    RUBY
+
+    expect_no_offenses(<<~RUBY)
+      change_column :questions, :archive_data, :jsonb, default: {}
+    RUBY
+  end
 end
